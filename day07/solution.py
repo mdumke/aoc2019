7a8ff349ed8202f9d6fb9b97e81b33f8b7a12060
Matthias Dumke <matthias.dumke@gmx.net>
2019-12-07 17:13:25 +0100

    refactor: remove unnecessary copying

--- day07/solution.py
+++ day07/solution.py
@@ -1,10 +1,10 @@
 """Compute maximum signal to generate through a chain of amplifiers."""
 
 import sys
-from thruster import *
+from thruster import find_max_signal
 
 if __name__ == '__main__':
     code = [int(n) for n in sys.stdin.readline().split(',')]
-    print('part 1', find_max_signal(code[:], [0, 1, 2, 3, 4]))
-    print('part 2', find_max_signal(code[:], [5, 6, 7, 8, 9]))
+    print('part 1', find_max_signal(code, [0, 1, 2, 3, 4]))
+    print('part 2', find_max_signal(code, [5, 6, 7, 8, 9]))
 

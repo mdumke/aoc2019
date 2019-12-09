8b29833a607d7b8f90f5de95c7395d134291d09d
Matthias Dumke <matthias.dumke@gmx.net>
2019-12-09 08:33:52 +0100

    add day 9: sensor boost

--- day09/main.py
+++ day09/main.py
@@ -7,10 +7,5 @@ from intcomputer import Intcomputer
 if __name__ == '__main__':
     code = [int(n) for n in sys.stdin.readline().strip().split(',')]
 
-    computer = Intcomputer(code)
-    _, keycode = computer.execute(1)
-    print('part1', keycode)
-
-    computer = Intcomputer(code)
-    _, coordinates = computer.execute(2)
-    print('part 2', coordinates)
+    print('part 1:', Intcomputer(code).execute(1)[1])
+    print('part 2:', Intcomputer(code).execute(2)[1])

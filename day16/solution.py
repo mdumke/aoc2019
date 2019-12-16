5fca277b9f57a61a2088e1f91754e684ab65b705
Matthias Dumke <matthias.dumke@gmx.net>
2019-12-16 15:48:44 +0100

    docs: add comment to justify fft2 usage

--- day16/solution.py
+++ day16/solution.py
@@ -16,6 +16,8 @@ if __name__ == '__main__':
     signal = input_ * 10000
     for _ in range(100):
         signal = fft2(signal)
+    # the read index refers to the second part of the signal,
+    # so using fft2 is valid
     i = int(''.join(map(str, input_[:7])))
     print('part 2:', signal[i:i + 8])
 
